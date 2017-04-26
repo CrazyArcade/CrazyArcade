c146eb43b89aa74cc464177c7b8b52f87cf3921a
giuem <i@giuem.com>
2017-04-26 23:20:13 +0800

    add settings

---
+++ Classes/Settings.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Settings
+{
+    namespace Font
+    {
+        namespace Size
+        {
+            constexpr int title  = 60;
+            constexpr int label  = 40;
+            constexpr int normal = 32;
+        }
+        namespace Type
+        {
+            constexpr char * title = "fonts/Montserrat-Medium.ttf";
+            constexpr char * base  = "fonts/OpenSans-Regular.ttf";
+        };
+    }
+}
