d9cec24f51a6c6c5ac53bf93f7ead033d310d1ee
giuem <i@giuem.com>
2017-04-26 22:43:58 +0800

    :lipstick:	change window size

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -3,7 +3,7 @@
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size designResolutionSize = cocos2d::Size(1280, 720);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
