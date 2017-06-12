d9700aa35b3043915bb4a3c236913e2dc647ef84
Sigen A <igulu1998@outlook.com>
2017-06-12 14:27:47 +0800

    :lipstick: remove banner

--- Classes/Scene/UI/ResultBox.cpp
+++ Classes/Scene/UI/ResultBox.cpp
@@ -3,22 +3,11 @@
 
 bool ResultBox::init()
 {
-    auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
-    pad = cocos2d::Sprite::create("Scene/PlayingPad.png");
-    pad->setPosition(visibleSize.width / 2, visibleSize.height*0.9);
-    pad->setScale(0.5);
-    auto label = cocos2d::Label::createWithTTF("IN GAME", Settings::Font::Type::base, Settings::Font::Size::title);
-    label->setPosition(pad->getContentSize().width / 2, pad->getContentSize().height / 2 - 5);
-    pad->addChild(label);
-    addChild(pad);
     return true;
 }
 
 void ResultBox::setResult(Result result)
 {
-     
-    pad->removeFromParentAndCleanup(true);
-    
     auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
     
     switch (result) {
