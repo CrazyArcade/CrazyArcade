84324f99b97a038bcad867e5593597437c73ee2a
sukamay <253246887@qq.com>
2017-06-13 23:20:45 +0800

    add back button

--- Classes/Scene/LoginScene.cpp
+++ Classes/Scene/LoginScene.cpp
@@ -1,5 +1,5 @@
 #include"LoginScene.h"
-#include"StartScene.h"
+
 
 
 USING_NS_CC;
@@ -32,11 +32,11 @@ bool LoginScene::init()
 	this->addChild(bg);
 
 	//log in item,username editbox input
-	auto editBoxSize = Size(visibleSize.width - 100, visibleSize.height * 0.1);
+	auto editBoxSize = Size(visibleSize.width/2, visibleSize.height * 0.1);
 	
-	std::string pNormalSprite = "edit.png";
-	_editUserName = cocos2d::ui::EditBox::create(editBoxSize + Size(0,40), cocos2d::ui::Scale9Sprite::create(pNormalSprite));
-	_editUserName->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 3 + origin.y));
+	std::string pNormalSprite = "RoomScene/c8.png";
+	_editUserName = cocos2d::ui::EditBox::create(editBoxSize + Size(0,20) , cocos2d::ui::Scale9Sprite::create(pNormalSprite));
+	_editUserName->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	_editUserName->setFontColor(Color3B::BLUE);
 	_editUserName->setPlaceHolder("Name:");
 	_editUserName->setPlaceholderFontColor(Color3B::WHITE);
@@ -48,8 +48,8 @@ bool LoginScene::init()
 
 	//password editbox
 	_editPassword = cocos2d::ui::EditBox::create(Size(editBoxSize.width,
-		editBoxSize.height +20),"edit.png");
-	_editPassword->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 4 + origin.y));
+		editBoxSize.height +20),"RoomScene/c8.png");
+	_editPassword->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y - editBoxSize.height*1.5));
 	_editPassword->setFontColor(Color3B::BLUE);
 	_editPassword->setPlaceHolder("Password:");
 	_editPassword->setMaxLength(20);
@@ -59,32 +59,66 @@ bool LoginScene::init()
 	this->addChild(_editPassword);
 	
 	//login menu
-	Menu* mn = Menu::createWithItem(nullptr);
+	auto loginItem = cocos2d::MenuItemImage::create(
+		"RoomScene/login.png",
+		"RoomScene/login.png");
+	auto loginToggleItem = cocos2d::MenuItemToggle::createWithCallback(
+		CC_CALLBACK_1(LoginScene::menuLoginCallback, this),
+		loginItem,
+		NULL
+	);
 
+	loginItem->setPosition(Director::getInstance()->convertToGL(Vec2(visibleSize.width/2,visibleSize.height*3/4)));
+
+    Menu * mn = Menu::create(loginToggleItem, NULL);
+	mn->setPosition(Vec2::ZERO);
 	this->addChild(mn);
+	addChild(createText());
 
 	return true;
 }
 void LoginScene::menuLoginCallback(cocos2d::Ref* pSender)
 {
-
-
+	auto scene = RoomScene::createScene();
+	Director::getInstance()->pushScene(scene);
 }
-void editBoxEditingDidBegin(cocos2d::ui::EditBox* editBox)
+void LoginScene::editBoxEditingDidBegin(cocos2d::ui::EditBox* editBox)
 {
 	log("editbox %p didbegin", editBox);
 }
-void editBoxEditingDidEnd(cocos2d::ui::EditBox* editBox)
+void LoginScene::editBoxEditingDidEnd(cocos2d::ui::EditBox* editBox)
 {
 
 }
-void editBoxTextChanged(
+void LoginScene::editBoxTextChanged(
 	cocos2d::ui::EditBox* editBox, const std::string &text)
 {
 
 }
-void editBoxReturn(cocos2d::ui::EditBox* editBox)
+void LoginScene::editBoxReturn(cocos2d::ui::EditBox* editBox)
 {
 
 
+}
+void LoginScene::menuBackCallback(cocos2d::Ref* pSender)
+{
+	Director::getInstance()->popScene();
+}
+cocos2d::Menu* LoginScene::createText() {
+	const auto buttons = Menu::create();
+
+	const auto backButton = cocos2d::MenuItemLabel::create(
+		Label::createWithTTF("Back", Settings::Font::Type::base, Settings::Font::Size::label),
+		CC_CALLBACK_1(LoginScene::menuBackCallback, this));
+
+	const auto visibleSize = Director::getInstance()->getVisibleSize();
+	const auto baseY = visibleSize.height * 0.85f;
+
+	backButton->setPosition(backButton->getContentSize().width / 2 + 30, baseY + 30);
+
+	buttons->addChild(backButton, 1);
+
+	buttons->setPosition(0, 0);
+
+	return buttons;
 }
--- Classes/Scene/LoginScene.h
+++ Classes/Scene/LoginScene.h
@@ -4,6 +4,9 @@
 
 #include<iostream>
 #include"cocos2d.h"
+#include"Settings.h"
+#include"SettingsScene.h"
+#include"RoomScene.h"
 #include"StartScene.h"
 #include"cocos2d/cocos/ui/CocosGUI.h"
 #include"cocos2d/extensions/cocos-ext.h"
@@ -14,9 +17,12 @@ class LoginScene :public cocos2d::Layer,public cocos2d::ui::EditBoxDelegate
 protected:
 	cocos2d::ui::EditBox* _editUserName;
 	cocos2d::ui::EditBox* _editPassword;
-	void menuLoginCallback(cocos2d::Ref* pSender, cocos2d::ui::Widget::TouchEventType type);
-
+	void menuLoginCallback(cocos2d::Ref* pSender);
+private:
+	cocos2d::Menu* createText();
+	void menuBackCallback(cocos2d::Ref* pSender);
 public :
+	
 	static cocos2d::Scene * createScene();
 	virtual bool init();
 	virtual void editBoxEditingDidBegin(cocos2d::ui::EditBox* editBox);
