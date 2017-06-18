88b218c3672273f5793c841710e558d439df1e7b
sukamay <253246887@qq.com>
2017-06-18 14:36:33 +0800

    add helpscene

--- Classes/Scene/HelpScene.cpp
+++ Classes/Scene/HelpScene.cpp
@@ -28,8 +28,122 @@ bool HelpScene::init()
 
     addChild(createText());
 
-    return true;
+	bg01Init();
+	helpOn = UserDefault::getInstance()->getIntegerForKey("helpOn", 0);
+	if (helpOn == 0) {
+		bg01Init();
+		createCloseButton();
+	}
+	else if (helpOn == 1) {
+		bg02Init();
+		createItem();
+	}
+	else {
+		bg03Init();
+		createItem();
+	}
+	return true;
 }
+void HelpScene::bg01Init()
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2  origin = Director::getInstance()->getVisibleOrigin();
+
+	//auto bg = Sprite::create("Scene/help04.png");
+	/*auto bg = MenuItemImage::create(
+	"Scene/help04.png",
+	"Scene/help04.png",
+	"Scene/help04.png",
+	CC_CALLBACK_1(HelpScene::menuHelp02Callback, this)
+	);*/
+	auto bgOn = cocos2d::MenuItemImage::create(
+		"Scene/help04.png",
+		"Scene/help04.png");
+	auto bgOff = bgOn;
+	auto bg = cocos2d::MenuItemToggle::createWithCallback(
+		CC_CALLBACK_1(HelpScene::menuHelp02Callback, this),
+		bgOn,
+		bgOff,
+		NULL);
+	bg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	bg->setScaleZ(1);
+	auto mn = Menu::create(bg, NULL);
+	mn->setPosition(Vec2::ZERO);
+
+	//this->addChild(bg);
+	this->addChild(mn);
+}
+
+void HelpScene::bg02Init()
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2  origin = Director::getInstance()->getVisibleOrigin();
+
+	auto bg = Sprite::create("Scene/help01.png");
+	bg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	bg->setScaleZ(2);
+	this->addChild(bg);
+}
+
+void  HelpScene::bg03Init()
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2  origin = Director::getInstance()->getVisibleOrigin();
+
+	auto bg = Sprite::create("Scene/help02.png");
+	bg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	bg->setScaleZ(3);
+	this->addChild(bg);
+}
+void HelpScene::createItem()
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	//last item
+	auto lastOnItem = MenuItemImage::create(
+		"Scene/helpLastOn.png",
+		"Scene/helpLastOn.png");
+	lastOnItem->setPosition(Vec2(visibleSize.width *0.5f, visibleSize.height * 0.3f));
+	auto lastOffItem = MenuItemImage::create(
+		"Scene/helpLastOff.png",
+		"Scene/helpLastOff.png");
+	auto lastToggleMenuItem = MenuItemToggle::createWithCallback(
+		CC_CALLBACK_1(HelpScene::menuHelp01Callback, this),
+		lastOnItem,
+		lastOffItem, NULL);
+	lastToggleMenuItem->setPosition(Vec2(visibleSize.width * 0.5f, visibleSize.height * 0.13f));
+	//next item 
+	auto nextOnItem = MenuItemImage::create(
+		"Scene/helpNextOn.png",
+		"Scene/helpNextOn.png");
+	auto nextOffItem = MenuItemImage::create(
+		"Scene/helpNextOff.png",
+		"Scene/helpNextOff.png");
+	auto nextToggleMenuItem = MenuItemToggle::createWithCallback(
+		CC_CALLBACK_1(HelpScene::menuHelp02Callback, this),
+		nextOnItem,
+		nextOffItem, NULL);
+	nextToggleMenuItem->setPosition(Vec2(visibleSize.width * 0.5f + lastToggleMenuItem->getContentSize().width, visibleSize.height * 0.13f));
+	//close item
+	auto closeItem = MenuItemImage::create(
+		"Scene/helpClose02.png",
+		"Scene/helpClose02.png",
+		"Scene/helpClose02.png",
+		CC_CALLBACK_1(HelpScene::menuBackCallback, this));
+	closeItem->setPosition(Vec2(visibleSize.width * 0.5f + lastToggleMenuItem->getContentSize().width * 2, visibleSize.height * 0.13f));
+	if (helpOn == 1) {
+		auto mn = Menu::create(lastOffItem, nextToggleMenuItem, closeItem, NULL);
+		mn->setPosition(Vec2::ZERO);
+		this->addChild(mn);
+	}
+	else {
+		auto mn = Menu::create(lastToggleMenuItem, nextToggleMenuItem, closeItem, NULL);
+		mn->setPosition(Vec2::ZERO);
+		this->addChild(mn);
+	}
+}
+
 
 cocos2d::Menu* HelpScene::createText() {                                
     const auto buttons = Menu::create();
@@ -49,8 +163,35 @@ cocos2d::Menu* HelpScene::createText() {
 
     return buttons;
 }
+void HelpScene::createCloseButton()
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+
+	auto closeButton = ui::Button::create("Scene/helpClose.png", "Scene/helpClose.png");
+	closeButton->setPosition(Vec2(visibleSize.width / 2, visibleSize.height * 0.15f));
+	closeButton->setOpacity(233);
+
+	closeButton->addTouchEventListener([=](Ref* sender, ui::Widget::TouchEventType type) {
+		if (type != ui::Widget::TouchEventType::ENDED) return;
+		Director::getInstance()->popScene();
+	});
+	this->addChild(closeButton);
+}
 
 void HelpScene::menuBackCallback(Ref* pSender)
 {
     Director::getInstance()->popScene();
+	UserDefault::getInstance()->setIntegerForKey("helpOn", 0);
+}
+void HelpScene::menuHelp01Callback(Ref* pSender)
+{
+	helpOn = UserDefault::getInstance()->getIntegerForKey("helpOn", 0);
+	UserDefault::getInstance()->setIntegerForKey("helpOn", --helpOn);
+	Director::getInstance()->replaceScene(TransitionFade::create(1.2f, HelpScene::createScene()));
+}
+void HelpScene::menuHelp02Callback(Ref* pSender)
+{
+	helpOn = UserDefault::getInstance()->getIntegerForKey("helpOn", 0);
+	UserDefault::getInstance()->setIntegerForKey("helpOn", ++helpOn);
+	Director::getInstance()->replaceScene(TransitionFade::create(1.2f, HelpScene::createScene()));
 }
--- Classes/Scene/HelpScene.h
+++ Classes/Scene/HelpScene.h
@@ -2,12 +2,22 @@
 #define __SCENE_HELPSCENE_H__
 
 #include "cocos2d.h"
+#include"ui/UIButton.h"
 
 class HelpScene : public cocos2d::Layer
 {
 private:
-    void menuBackCallback(cocos2d::Ref* pSender);
-    cocos2d::Menu* createText();
+	void menuBackCallback(cocos2d::Ref* pSender);
+	//void menuCloseCallback(cocos2d::Ref* pSender);
+	void menuHelp01Callback(cocos2d::Ref* pSender);
+	void menuHelp02Callback(cocos2d::Ref* pSender);
+	void bg01Init();
+	void bg02Init();
+	void bg03Init();
+	cocos2d::Menu* createText();
+	void createCloseButton();
+	void createItem();
+	int  helpOn;
 public:
     static cocos2d::Scene* createScene();
     virtual bool init();
